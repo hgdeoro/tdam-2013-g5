8421993d97cc7c53b7de6cd1ba5d8b82add8c97e
hgdeoro@gmail.com <hgdeoro@gmail.com@0117bdac-9510-e2bd-253c-24df4fda417d>
2013-08-28 19:14:43 +0000

    Contactos (pick) y agregado
    git-svn-id: https://tdam-2013-g5.googlecode.com/svn/trunk@13 0117bdac-9510-e2bd-253c-24df4fda417d

--- HolaMundo/src/ar/com/hgdeoro/tdam/holamundo/MainActivity.java
+++ HolaMundo/src/ar/com/hgdeoro/tdam/holamundo/MainActivity.java
@@ -1,7 +1,13 @@
 package ar.com.hgdeoro.tdam.holamundo;
 
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
+import android.provider.ContactsContract;
+import android.provider.ContactsContract.CommonDataKinds.Phone;
+import android.provider.ContactsContract.Contacts.Data;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -10,44 +16,135 @@ import android.widget.EditText;
 
 public class MainActivity extends MyBaseActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    private static final int PICK_CONTACT = 1;
 
-		((Button) findViewById(R.id.btWasteMemory))
-				.setOnClickListener(new OnClickListener() {
+    private void addStatus(String status) {
+        EditText text = (EditText) findViewById(R.id.status);
+        text.setText(text.getText() + status + "\n");
+    }
 
-					@Override
-					public void onClick(View v) {
-						EditText text = (EditText) findViewById(R.id.status);
-						StringBuffer sb = new StringBuffer();
-						for (int i = 0; i < 1000; i++)
-							sb.append("Jo jo jo! Feliz festivus! ");
-						text.setText(text.getText() + sb.toString());
-						Log.i(this.getClass().getName(), "Add: " + sb.length()
-								+ " bytes?");
-					}
+    private void cleanStatus() {
+        ((EditText) findViewById(R.id.status)).setText("");
+    }
 
-				});
+    private void setHelp() {
+        addStatus("1. Abrir pagina web");
+        addStatus("2. Seleccionar contacto");
+        addStatus("3. Insertar contacto");
+        addStatus("4. ");
+    }
 
-		((Button) findViewById(R.id.btClean))
-				.setOnClickListener(new OnClickListener() {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-					@Override
-					public void onClick(View v) {
-						((EditText) findViewById(R.id.status)).setText("");
-					}
+        ((Button) findViewById(R.id.btHelp)).setOnClickListener(new OnClickListener() {
 
-				});
+            @Override
+            public void onClick(View v) {
+                cleanStatus();
+                setHelp();
+            }
 
-	}
+        });
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+        ((Button) findViewById(R.id.btClean)).setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                cleanStatus();
+            }
+
+        });
+
+        /*
+         * Intent 1
+         */
+        ((Button) findViewById(R.id.btIntent1)).setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                addStatus("Abriendo 'http://lwn.net'");
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://lwn.net"));
+                startActivity(intent);
+            }
+
+        });
+
+        /*
+         * Intent 2
+         */
+        ((Button) findViewById(R.id.btIntent2)).setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, Uri
+                        .parse("content://contacts/people"));
+                startActivityForResult(intent, PICK_CONTACT);
+            }
+
+        });
+
+        /*
+         * Intent 3
+         */
+        ((Button) findViewById(R.id.btIntent3)).setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                ContentValues values = new ContentValues();
+                values.put(Data.RAW_CONTACT_ID, Long.valueOf(-1));
+                values.put(Data.MIMETYPE, Phone.CONTENT_ITEM_TYPE);
+                values.put(Phone.NUMBER, "54 9 351 9991111");
+                values.put(Phone.TYPE, Phone.TYPE_CUSTOM);
+                values.put(Phone.LABEL, "Some Contact");
+                Uri dataUri = getContentResolver()
+                        .insert(ContactsContract.Data.CONTENT_URI, values);
+            }
+
+        });
+
+        /*
+         * Intent 4
+         */
+        ((Button) findViewById(R.id.btIntent4)).setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+            }
+
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+        case (PICK_CONTACT):
+            addStatus("onActivityResult(PICK_CONTACT)");
+            if (resultCode == Activity.RESULT_OK) {
+                Uri contactData = data.getData();
+                addStatus("onActivityResult(PICK_CONTACT) -> contactData: " + contactData);
+
+            } else {
+                addStatus("onActivityResult(PICK_CONTACT) -> resultCode: " + resultCode);
+
+            }
+        default:
+            addStatus("onActivityResult() -> requestCode desconocido: " + requestCode);
+            break;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
 }
