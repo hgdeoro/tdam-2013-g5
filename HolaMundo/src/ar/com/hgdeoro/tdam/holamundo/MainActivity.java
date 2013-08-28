ad44edf5c63dbc4421814f7d8a029999a9b93f1a
hgdeoro@gmail.com <hgdeoro@gmail.com@0117bdac-9510-e2bd-253c-24df4fda417d>
2013-08-28 16:55:37 +0000

    Agregue callback
    git-svn-id: https://tdam-2013-g5.googlecode.com/svn/trunk@10 0117bdac-9510-e2bd-253c-24df4fda417d

--- HolaMundo/src/ar/com/hgdeoro/tdam/holamundo/MainActivity.java
+++ HolaMundo/src/ar/com/hgdeoro/tdam/holamundo/MainActivity.java
@@ -1,7 +1,12 @@
 package ar.com.hgdeoro.tdam.holamundo;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends MyBaseActivity {
 
@@ -9,6 +14,23 @@ public class MainActivity extends MyBaseActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		Button button = (Button) findViewById(R.id.wasteMoreMemoryButton);
+		button.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				EditText text = (EditText) findViewById(R.id.status);
+				StringBuffer sb = new StringBuffer();
+				for (int i = 0; i < 1000; i++)
+					sb.append("Jo jo jo! Feliz festivus! ");
+				text.setText(text.getText() + sb.toString());
+				Log.i(this.getClass().getName(), "Add: " + sb.length()
+						+ " bytes?");
+			}
+
+		});
+
 	}
 
 	@Override
