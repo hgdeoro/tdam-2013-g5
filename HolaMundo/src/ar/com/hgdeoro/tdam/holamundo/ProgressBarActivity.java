5bf983aee9b7cd8af4780995e156def79d8ac3c5
hgdeoro@gmail.com <hgdeoro@gmail.com@0117bdac-9510-e2bd-253c-24df4fda417d>
2013-08-29 03:01:00 +0000

    Ejemplo de ProgressBar
    git-svn-id: https://tdam-2013-g5.googlecode.com/svn/trunk@24 0117bdac-9510-e2bd-253c-24df4fda417d

--- HolaMundo/src/ar/com/hgdeoro/tdam/holamundo/ProgressBarActivity.java
+++ HolaMundo/src/ar/com/hgdeoro/tdam/holamundo/ProgressBarActivity.java
@@ -1,6 +1,10 @@
 package ar.com.hgdeoro.tdam.holamundo;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
 public class ProgressBarActivity extends MyBaseActivity {
 
@@ -8,7 +12,20 @@ public class ProgressBarActivity extends MyBaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.progressbar);
-
+        ((Button) findViewById(R.id.btMas)).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ProgressBar pb = (ProgressBar) findViewById(R.id.progressBar);
+                pb.setProgress(pb.getProgress() + 10);
+            }
+        });
+        ((Button) findViewById(R.id.btMenos)).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ProgressBar pb = (ProgressBar) findViewById(R.id.progressBar);
+                pb.setProgress(pb.getProgress() - 10);
+            }
+        });
     }
 
 }
