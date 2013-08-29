365456b5936342a66cc14cd08c733e1bcea4d407
hgdeoro@gmail.com <hgdeoro@gmail.com@0117bdac-9510-e2bd-253c-24df4fda417d>
2013-08-29 01:37:22 +0000

    [Ejercicio1] Agrega listeners
    git-svn-id: https://tdam-2013-g5.googlecode.com/svn/trunk@19 0117bdac-9510-e2bd-253c-24df4fda417d

--- Ejercicio01/src/ar/com/hgdeoro/tdam/ejercicio01/MainActivity.java
+++ Ejercicio01/src/ar/com/hgdeoro/tdam/ejercicio01/MainActivity.java
@@ -3,18 +3,43 @@ package ar.com.hgdeoro.tdam.ejercicio01;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+
+    private void setText(CharSequence newText) {
+        ((EditText) findViewById(R.id.editText)).setText(newText);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        OnClickListener onClickListener = new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                setText(((Button) v).getText());
+            }
+        };
+
+        ((Button) findViewById(R.id.btArriba)).setOnClickListener(onClickListener);
+        ((Button) findViewById(R.id.btAbajoIzq)).setOnClickListener(onClickListener);
+        ((Button) findViewById(R.id.btAbajoDer)).setOnClickListener(onClickListener);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setText("");
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
