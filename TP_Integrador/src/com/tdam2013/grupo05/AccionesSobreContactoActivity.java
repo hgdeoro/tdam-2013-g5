de1e7ea48f2a6b9f5a865485b673d6b9076fd7be
hgdeoro@gmail.com <hgdeoro@gmail.com@0117bdac-9510-e2bd-253c-24df4fda417d>
2013-09-13 23:33:11 +0000

    Agrega activity para acciones sobre contacto
    git-svn-id: https://tdam-2013-g5.googlecode.com/svn/trunk@47 0117bdac-9510-e2bd-253c-24df4fda417d

---
+++ TP_Integrador/src/com/tdam2013/grupo05/AccionesSobreContactoActivity.java
@@ -0,0 +1,31 @@
+package com.tdam2013.grupo05;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+
+public class AccionesSobreContactoActivity extends ListActivity {
+
+    public static final String[] acciones = new String[] { "Llamar", "Llamar", "Llamar",
+            "Enviar SMS", "Enviar SMS", "Enviar Weekeeteekee", };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acciones_sobre_contacto_activity);
+
+        setListAdapter(new ArrayAdapter<String>(this,
+                R.layout.acciones_sobre_contacto_activity_item,
+                R.id.acciones_sobre_contacto_item_accion, acciones));
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.acciones_sobre_contacto, menu);
+        return true;
+    }
+
+}
--- TP_Integrador/src/com/tdam2013/grupo05/ListaDeContactosActivity.java
+++ TP_Integrador/src/com/tdam2013/grupo05/ListaDeContactosActivity.java
@@ -1,9 +1,13 @@
 package com.tdam2013.grupo05;
 
 import android.app.ListActivity;
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ListaDeContactosActivity extends ListActivity {
 
@@ -24,6 +28,15 @@ public class ListaDeContactosActivity extends ListActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.lista_de_contactos, menu);
         return true;
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        Intent intent = new Intent();
+        intent.setComponent(new ComponentName(this,
+                "com.tdam2013.grupo05.AccionesSobreContactoActivity"));
+        this.startActivity(intent);
     }
 
 }
