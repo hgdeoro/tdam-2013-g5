041314e6d89dddbd449da3b2f7539af612e97af8
hgdeoro@gmail.com <hgdeoro@gmail.com@0117bdac-9510-e2bd-253c-24df4fda417d>
2013-09-13 23:32:20 +0000

    Agregue ListView
    git-svn-id: https://tdam-2013-g5.googlecode.com/svn/trunk@45 0117bdac-9510-e2bd-253c-24df4fda417d

--- TP_Integrador/src/com/tdam2013/grupo05/ListaDeContactosActivity.java
+++ TP_Integrador/src/com/tdam2013/grupo05/ListaDeContactosActivity.java
@@ -1,15 +1,22 @@
 package com.tdam2013.grupo05;
 
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
 
-public class ListaDeContactosActivity extends Activity {
+public class ListaDeContactosActivity extends ListActivity {
+
+    public static final String[] contactos = new String[] { "Juan", "Pepe", "Juan", "Pepe", "Juan",
+            "Pepe", "Juan", "Pepe", "Juan", "Pepe", "Juan", "Pepe", "Juan", "Pepe", "Juan", "Pepe",
+            "Juan", "Pepe", "Juan", "Pepe", "Juan", "Pepe", };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lista_de_contactos);
+        setContentView(R.layout.lista_de_contactos_activity);
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.lista_de_contactos_activity_item,
+                R.id.lista_de_contactos_item_label, contactos));
     }
 
     @Override
