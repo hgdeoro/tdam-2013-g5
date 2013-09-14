5dfe941db7a5b094da79e1c1e4ed6d393dbb1adc
hgdeoro@gmail.com <hgdeoro@gmail.com@0117bdac-9510-e2bd-253c-24df4fda417d>
2013-09-14 01:22:57 +0000

    Agrega dialogo
    git-svn-id: https://tdam-2013-g5.googlecode.com/svn/trunk@52 0117bdac-9510-e2bd-253c-24df4fda417d

--- TP_Integrador/src/com/tdam2013/grupo05/EnviarMensajeWeb.java
+++ TP_Integrador/src/com/tdam2013/grupo05/EnviarMensajeWeb.java
@@ -1,15 +1,29 @@
 package com.tdam2013.grupo05;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class EnviarMensajeWeb extends Activity {
+
+    public static final int DIALOG = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.enviar_mensaje_web_activity);
+
+        ((Button) this.findViewById(R.id.enviar_mensaje_web_button))
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        EnviarMensajeWeb.this.showDialog(DIALOG);
+                    }
+                });
     }
 
     @Override
@@ -17,6 +31,16 @@ public class EnviarMensajeWeb extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.enviar_mensaje_web, menu);
         return true;
+    }
+
+    protected Dialog onCreateDialog(int id) {
+        if (id == DIALOG) {
+            Dialog dialog = new AlertDialog.Builder(this).setTitle("ERROR")
+                    .setMessage("No se ha podido enviar el mensaje web.").create();
+            return dialog;
+        } else {
+            return super.onCreateDialog(id);
+        }
     }
 
 }
