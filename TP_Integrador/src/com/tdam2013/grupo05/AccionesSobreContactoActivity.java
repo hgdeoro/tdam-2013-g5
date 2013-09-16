1713efe68babe846765f52b1ecf4a91503b92e3f
hgdeoro@gmail.com <hgdeoro@gmail.com@0117bdac-9510-e2bd-253c-24df4fda417d>
2013-09-16 16:34:50 +0000

    Uso intents para realizar acciones sobre contacto
    git-svn-id: https://tdam-2013-g5.googlecode.com/svn/trunk@62 0117bdac-9510-e2bd-253c-24df4fda417d

--- TP_Integrador/src/com/tdam2013/grupo05/AccionesSobreContactoActivity.java
+++ TP_Integrador/src/com/tdam2013/grupo05/AccionesSobreContactoActivity.java
@@ -3,12 +3,19 @@ package com.tdam2013.grupo05;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class AccionesSobreContactoActivity extends ListActivity {
 
-    public static final String[] acciones = new String[] { "Llamar", "Llamar", "Enviar sms",
-            "Enviar email", "Enviar mensaje web", };
+    public static final String LLAMAR = "Llamar";
+    public static final String SMS = "Enviar SMS";
+    public static final String EMAIL = "Enviar email";
+    public static final String MSGWEB = "Enviar mensaje web";
+
+    public static final String[] acciones = new String[] { LLAMAR, LLAMAR, SMS, SMS, EMAIL, MSGWEB };
 
     // TODO: crear custom adaptar para cargar ambos textos: accion y dato
     // TODO: implementar onListItemClick() para respoder a clics
@@ -23,10 +30,37 @@ public class AccionesSobreContactoActivity extends ListActivity {
                 R.id.acciones_sobre_contacto_item_accion, acciones));
     }
 
+    /**
+     * List Item
+     */
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        final String accion = acciones[position];
+        if (LLAMAR.equals(accion)) {
+            this.startActivity(Utiles.getCallPhoneIntent("0"));
+
+        } else if (SMS.equals(accion)) {
+            this.startActivity(Utiles.getSendSmsIntent("0"));
+
+        } else if (EMAIL.equals(accion)) {
+            this.startActivity(Utiles.getSendEmailIntent("alguien@example.com"));
+
+        } else if (MSGWEB.equals(accion)) {
+            this.startActivity(Utiles.getEnviarMensajeWebActivityIntent(this));
+
+        } else {
+
+            Toast.makeText(getBaseContext(), "Accion desconocida", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Menu
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.acciones_sobre_contacto, menu);
+        // getMenuInflater().inflate(R.menu.acciones_sobre_contacto, menu);
         return true;
     }
 
--- TP_Integrador/src/com/tdam2013/grupo05/Utiles.java
+++ TP_Integrador/src/com/tdam2013/grupo05/Utiles.java
@@ -3,6 +3,7 @@ package com.tdam2013.grupo05;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 
 public class Utiles {
 
@@ -23,6 +24,42 @@ public class Utiles {
         Intent intent = new Intent();
         intent.setComponent(new ComponentName(ctx, AccionesSobreContactoActivity.class
                 .getCanonicalName()));
+        return intent;
+    }
+
+    public static Intent getEnviarMensajeWebActivityIntent(Context ctx) {
+        Intent intent = new Intent();
+        intent.setComponent(new ComponentName(ctx, EnviarMensajeWebActivity.class
+                .getCanonicalName()));
+        return intent;
+    }
+
+    /*
+     * Android
+     */
+    public static Intent getCallPhoneIntent(String numero) {
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        // intent.setData(Uri.parse("tel:" + numero));
+        intent.setData(Uri.fromParts("tel", numero, null));
+        return intent;
+    }
+
+    /*
+     * Android
+     */
+    public static Intent getSendSmsIntent(String numero) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        // intent.setData(Uri.parse("sms:" + numero));
+        intent.setData(Uri.fromParts("sms", numero, null));
+        return intent;
+    }
+
+    /*
+     * Android
+     */
+    public static Intent getSendEmailIntent(String to) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.fromParts("mailto", to, null));
         return intent;
     }
 
