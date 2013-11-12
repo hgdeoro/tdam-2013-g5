4604267e17ce9f3e3433c879439bfd813f3024a0
hgdeoro@gmail.com <hgdeoro@gmail.com@0117bdac-9510-e2bd-253c-24df4fda417d>
2013-11-12 22:12:45 +0000

    Tests de http client
    git-svn-id: https://tdam-2013-g5.googlecode.com/svn/trunk@106 0117bdac-9510-e2bd-253c-24df4fda417d

---
+++ Ejercicio01/src/ar/com/hgdeoro/tdam/ejercicio01/net/TestMensajeWeb.java
@@ -0,0 +1,40 @@
+package ar.com.hgdeoro.tdam.ejercicio01.net;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+public class TestMensajeWeb {
+
+	public static void main(String args[]) throws ClientProtocolException,
+			IOException {
+
+		HttpClient client = new DefaultHttpClient();
+		HttpPost post = new HttpPost("http://localhost:8080/messages");
+		HttpResponse response = client.execute(post);
+		response.getStatusLine().toString();
+		HttpEntity entity = response.getEntity();
+
+		if (entity != null) {
+			InputStream instream = entity.getContent();
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					instream));
+			String line = br.readLine();
+			while (line != null) {
+				System.out.println(line);
+				line = br.readLine();
+			}
+
+			instream.close();
+		}
+
+	}
+}
