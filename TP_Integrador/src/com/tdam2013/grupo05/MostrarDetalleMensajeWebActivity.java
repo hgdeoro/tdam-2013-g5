c1e4d52c0a425d56a92d3a667c6342cffc0b0471
hgdeoro@gmail.com <hgdeoro@gmail.com@0117bdac-9510-e2bd-253c-24df4fda417d>
2014-09-25 14:30:16 +0000

    Implementa UI de borrado de elemento de historial
    git-svn-id: https://tdam-2013-g5.googlecode.com/svn/trunk@156 0117bdac-9510-e2bd-253c-24df4fda417d

--- TP_Integrador/src/com/tdam2013/grupo05/MostrarDetalleMensajeWebActivity.java
+++ TP_Integrador/src/com/tdam2013/grupo05/MostrarDetalleMensajeWebActivity.java
@@ -3,8 +3,12 @@ package com.tdam2013.grupo05;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.tdam2013.grupo05.db.Database;
 import com.tdam2013.grupo05.db.MensajeWebDto;
@@ -35,12 +39,27 @@ public class MostrarDetalleMensajeWebActivity extends Activity {
 		db.close();
 	}
 
-	/*
-	 * PARA FINAL: servirá para eliminar mensaje!
-	 * 
-	 * @Override public boolean onCreateOptionsMenu(Menu menu) { // Inflate the
-	 * menu; this adds items to the action bar if it is present.
-	 * getMenuInflater().inflate(R.menu.mostrar_detalle_mensaje_web, menu);
-	 * return true; }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) { 
+		getMenuInflater().inflate(R.menu.mostrar_detalle_mensaje_web, menu);
+		return true;
+	}
+	
+	/**
+	 * Menu
 	 */
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+		Log.i("onMenuItemSelected()", "item: " + item);
+
+		if (item.getItemId() == R.id.action_borrar_item_historial) {
+			// FIXME: implementar borrado
+			Toast.makeText(getBaseContext(), "BORRANDO ITEM...", Toast.LENGTH_SHORT).show();
+		}
+
+		Log.i("onMenuItemSelected()", "Item no manejado "
+				+ "(puede deberse a que se utilizo el menu contextual)");
+		return super.onMenuItemSelected(featureId, item);
+
+	}	
 }
