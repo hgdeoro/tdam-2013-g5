ef9b6737513958965e5097a0e90463ea0a889227
hgdeoro@gmail.com <hgdeoro@gmail.com@0117bdac-9510-e2bd-253c-24df4fda417d>
2014-10-07 03:43:27 +0000

    Implementa logica de IngresarUsuarioDeContactoActivity
    git-svn-id: https://tdam-2013-g5.googlecode.com/svn/trunk@196 0117bdac-9510-e2bd-253c-24df4fda417d

--- TP_Integrador/src/com/tdam2013/grupo05/IngresarUsuarioDeContactoActivity.java
+++ TP_Integrador/src/com/tdam2013/grupo05/IngresarUsuarioDeContactoActivity.java
@@ -1,42 +1,78 @@
 package com.tdam2013.grupo05;
 
-import com.tdam2013.grupo05.RegistrarUsuarioActivity.RegisterUserTask;
-import com.tdam2013.grupo05.utiles.UtilesMensajesWeb;
-
 import android.app.Activity;
-import android.os.AsyncTask;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.tdam2013.grupo05.db.Database;
+import com.tdam2013.grupo05.utiles.UtilesIntents;
+import com.tdam2013.grupo05.utiles.UtilesMensajesWeb;
+
 public class IngresarUsuarioDeContactoActivity extends Activity {
+
+	private Long contactId = null;
+	private String displayName = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.ingresar_nombre_usuario_de_contacto_activity);
 
+		// Reusamos constantes de AccionesSobreContactoActivity
+		contactId = this.getIntent().getExtras()
+				.getLong(AccionesSobreContactoActivity.CONTACT_ID);
+		displayName = this.getIntent().getExtras()
+				.getString(AccionesSobreContactoActivity.DISPLAY_NAME);
+
 		getButton().setOnClickListener(new View.OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 
-				String username = getEditText().getText().toString().trim();
+				final String username = getEditText().getText().toString()
+						.trim();
 				if (UtilesMensajesWeb.usernameIsValid(username)) {
 
-					Toast.makeText(IngresarUsuarioDeContactoActivity.this,
-							"NOMBRE VALIDO! :_D", Toast.LENGTH_SHORT).show();
+					Log.i("IngresarUsuarioDeContactoActivity",
+							"Guardando nombre de usuario:  " + username
+									+ " para contacto: " + contactId);
+
+					// FIXME: esto se hace en main-thread. Pero solo un
+					// select + update/insert
+					new Database(IngresarUsuarioDeContactoActivity.this)
+							.updateInsertUsernameDeContacto(contactId, username);
+
+					Intent intentNewActivity = UtilesIntents
+							.getEnviarMensajeWebActivityIntent(
+									IngresarUsuarioDeContactoActivity.this,
+									username, contactId, displayName);
+
+					// Necesitamos FLAG_ACTIVITY_CLEAR_TOP para que el back
+					// stack quede bien seteado, y no quede en el back stack la
+					// activity que muestra el detalle del mensaje recien
+					// borrado!
+
+					intentNewActivity.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+					IngresarUsuarioDeContactoActivity.this
+							.startActivity(intentNewActivity);
 
 				} else {
-					Toast.makeText(IngresarUsuarioDeContactoActivity.this,
-							R.string.nombre_de_usuario_invalido,
-							Toast.LENGTH_SHORT).show();
+					mostrarErrorNombreDeUsuarioInvalido();
 				}
 
 			}
 		});
+	}
+
+	private void mostrarErrorNombreDeUsuarioInvalido() {
+		Toast.makeText(IngresarUsuarioDeContactoActivity.this,
+				R.string.nombre_de_usuario_invalido, Toast.LENGTH_SHORT).show();
+
 	}
 
 	private Button getButton() {
