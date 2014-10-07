5ffa8fb37d367bfeb7cf639ed2431e52bbbb0f09
hgdeoro@gmail.com <hgdeoro@gmail.com@0117bdac-9510-e2bd-253c-24df4fda417d>
2014-10-07 13:39:39 +0000

    Tercera implementacion de notificaciones
    git-svn-id: https://tdam-2013-g5.googlecode.com/svn/trunk@213 0117bdac-9510-e2bd-253c-24df4fda417d

--- TP_Integrador/src/com/tdam2013/grupo05/utiles/UtilesNotifications.java
+++ TP_Integrador/src/com/tdam2013/grupo05/utiles/UtilesNotifications.java
@@ -2,6 +2,8 @@ package com.tdam2013.grupo05.utiles;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.support.v4.app.NotificationCompat;
 
@@ -44,6 +46,8 @@ public class UtilesNotifications {
 		ctx = ctx.getApplicationContext();
 
 		//
+		// ----------
+		//
 		// Hay 2 implementaciones porque con la implementación inicial, luego de
 		// mostrar y ocultar la notification bar un par de veces, esta se
 		// bloquea oculta, y no puede volver a mostrarse.
@@ -53,24 +57,25 @@ public class UtilesNotifications {
 		// -
 		// https://stackoverflow.com/questions/17291699/android-emulator-only-opens-notification-bar-once
 		//
+		// ----------
+		//
+		// Luego de probar con una 3ra implementacion, hice la prueba de bajar y
+		// subir el area de notificaciones en el emulador, y se bloquea! Aun
+		// antes de mostrar ninguna notificacion. El problema definitivamente es
+		// el emulador y no la app.
+		//
+		// ----------
+		//
 
 		if (false) {
 
-			// String tickerText = ctx.getString(R.string.xxxxxxxxxxxxxxxxx);
+			/*
+			 * PRIMERA IMPLEMENTACION. La barra se bloquea
+			 */
 
 			Notification notification = new Notification(
 					R.drawable.ic_launcher, tickerText,
 					System.currentTimeMillis());
-
-			// PendingIntent pendingIntent = PendingIntent.getActivity(ctx, 0,
-			// Utilities.getOpenApplicationIntent(ctx), 0);
-
-			// notification
-			// .setLatestEventInfo(
-			// ctx,
-			// ctx.getString(R.string.title_notification_disconnection),
-			// ctx.getString(R.string.message_notification_disconnection),
-			// pendingIntent);
 
 			notification.setLatestEventInfo(ctx, contentTitle, contentText,
 					null);
@@ -78,7 +83,13 @@ public class UtilesNotifications {
 
 			getManager(ctx).notify(notificationId, notification);
 
-		} else {
+		}
+
+		if (false) {
+
+			/*
+			 * SEGUNDA IMPLEMENTACION. La barra se bloquea
+			 */
 
 			NotificationCompat.Builder builder = new NotificationCompat.Builder(
 					ctx);
@@ -95,6 +106,48 @@ public class UtilesNotifications {
 			notification.flags |= Notification.FLAG_AUTO_CANCEL;
 
 			getManager(ctx).notify(notificationId, notification);
+		}
+
+		if (true) {
+
+			/*
+			 * TERCERA IMPLEMENTACION. La barra se bloquea.
+			 */
+
+			// NotificationCompat.Builder mBuilder = new
+			// NotificationCompat.Builder(
+			// ctx).setSmallIcon(R.drawable.notification_icon)
+			// .setContentTitle("My notification")
+			// .setContentText("Hello World!");
+
+			NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(
+					ctx).setContentTitle(contentTitle).setContentText(
+					contentText);
+
+			// Creates an explicit intent for an Activity in your app
+			// Intent resultIntent = new Intent(ctx, ResultActivity.class);
+
+			// The stack builder object will contain an artificial back stack
+			// for the started Activity.
+			// This ensures that navigating backward from the Activity leads out
+			// of your application to the Home screen.
+
+			TaskStackBuilder stackBuilder = TaskStackBuilder.create(ctx);
+
+			// Adds the back stack for the Intent (but not the Intent itself)
+			// stackBuilder.addParentStack(ResultActivity.class);
+
+			// Adds the Intent that starts the Activity to the top of the stack
+			// stackBuilder.addNextIntent(resultIntent);
+
+			PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(
+					0, PendingIntent.FLAG_UPDATE_CURRENT);
+			mBuilder.setContentIntent(resultPendingIntent);
+			NotificationManager mNotificationManager = (NotificationManager) ctx
+					.getSystemService(Context.NOTIFICATION_SERVICE);
+
+			// mId allows you to update the notification later on.
+			mNotificationManager.notify(notificationId, mBuilder.build());
 
 		}
 
